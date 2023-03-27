0d94807cc27c250f7b2fa409158dccfc06c1ed72
Ernesto Gonzalez <egonzalez442@gmail.com>
2023-03-27 19:36:25 -0400

    Adds proper display to content

---
+++ frontend/components/list view/ListView.jsx
@@ -0,0 +1,11 @@
+import styles from './listview.module.css';
+
+const ListView = () => {
+  return (
+    <div className={styles.ListView}>
+      <button>Random Color</button>
+    </div>
+  );
+};
+
+export default ListView;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,24 @@ import { useState } from 'react';
 import styles from './App.module.css';
 import Header from '../components/header/Header';
 import Sidenav from '../components/sidenav/Sidenav';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import ListView from '../components/list view/ListView';
 
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
     <div className={styles.App}>
-      <Header />
-      <Sidenav />
+      <BrowserRouter>
+        <Header />
+        <div className={styles.content}>
+          <Sidenav />
+          <Routes>
+            <Route exact path="/" element={<ListView />} />
+            <Route exact path="/display" />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
