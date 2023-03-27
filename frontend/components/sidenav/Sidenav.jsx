4320716392331a18c9381a7c66b3d434c7614c54
Ernesto Gonzalez <egonzalez442@gmail.com>
2023-03-27 18:32:18 -0400

    Slight refactor and adds folder for sidenav

---
+++ frontend/components/sidenav/Sidenav.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+import styles from './sidenav.module.css';
+
+const Sidenav = () => {
+  return (
+  <div className={styles.Sidenav}>
+    <button>Random Color</button>
+  </div>
+  );
+};
+
+export default Sidenav;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import './App.css';
-import Header from '../components/Header';
+import Header from '../components/header/Header';
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <div className="App">
