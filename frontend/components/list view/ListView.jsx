5ed46a8088f6074713ae99e9741d0fd98c245688
Ernesto Gonzalez <egonzalez442@gmail.com>
2023-03-28 03:21:09 -0400

    Adds hex content and arrows

--- frontend/components/list view/ListView.jsx
+++ frontend/components/list view/ListView.jsx
@@ -1,9 +1,38 @@
 import styles from './listview.module.css';
+import { randomHex } from '../../utils/helper';
+import { useState } from 'react';
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from 'react-icons/bs';
+
+function createHexCache() {
+  const hexCache = [];
+  for (let j = 0; j < 12; j++) {
+    hexCache.push(randomHex());
+  }
+  return hexCache;
+}
 
 const ListView = () => {
+  const [hexCache, setHexcache] = useState(() => createHexCache());
+
   return (
     <div className={styles.ListView}>
-      <button>Random Color</button>
+      {hexCache.map((el, idx) => {
+        return (
+          <div key={`hex-key-${idx}`} className={styles.hex}>
+            <div
+              className={styles.hexColor}
+              style={{
+                backgroundColor: `${el}`,
+              }}
+            ></div>
+            <button>{el}</button>
+          </div>
+        );
+      })}
+      {/* Comment */}
+      <div className={styles.arrowKeys}>
+        {<BsArrowLeftCircleFill />}0{<BsArrowRightCircleFill />}
+      </div>
     </div>
   );
 };
