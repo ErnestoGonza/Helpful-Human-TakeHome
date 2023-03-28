d7e9d6c13a6f30d8acc4fb166e324d4d9b94a4c4
Ernesto Gonzalez <egonzalez442@gmail.com>
2023-03-28 19:20:32 -0400

    Adds functionality for the home button

--- frontend/components/header/Header.jsx
+++ frontend/components/header/Header.jsx
@@ -1,11 +1,14 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import styles from './header.module.css';
 import mySVG from './logo-symbol.svg';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <div className={styles.header}>
-      <img src={mySVG}></img>
+      <img src={mySVG} onClick={() => navigate('/')}></img>
       <input type="text" placeholder="Search" />
     </div>
   );
