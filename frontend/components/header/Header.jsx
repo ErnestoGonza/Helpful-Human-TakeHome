63d5a813f9de20b0c1fee77217d5f80017753100
Ernesto Gonzalez <egonzalez442@gmail.com>
2023-03-29 10:22:28 -0400

    Adds functionality to search bar!

--- frontend/components/header/Header.jsx
+++ frontend/components/header/Header.jsx
@@ -2,14 +2,34 @@ import React from 'react';
 import styles from './header.module.css';
 import mySVG from './logo-symbol.svg';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import stc from 'string-to-color';
 
 const Header = () => {
   const navigate = useNavigate();
+  const [inputValue, setInputValue] = useState();
+
+  function handleClick(event) {
+    if (event.key === 'Enter') {
+      const hex = stc(inputValue);
+      setInputValue('');
+      navigate('display', { state: { hex: hex } });
+    }
+  }
+
+  function handleChange(event) {
+    setInputValue(event.target.value);
+  }
 
   return (
     <div className={styles.header}>
       <img src={mySVG} onClick={() => navigate('/')}></img>
-      <input type="text" placeholder="Search" />
+      <input
+        type="text"
+        placeholder="Search"
+        onChange={handleChange}
+        onKeyDown={handleClick}
+      />
     </div>
   );
 };
